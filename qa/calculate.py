6bb57cd6689adcd347952203fe9aab99471f3ee3
amclio <lincroe@protonmail.com>
2023-06-25 15:40:37 +0900

    Fix logic that calculate final score

--- qa/calculate.py
+++ qa/calculate.py
@@ -1,14 +1,25 @@
 from deal_with_csv import format_csv
+import pandas as pd
 
 
 def count_overall_systems_with_highest_avg_fscore(df):
     overall_result_dict = {}
     grouped_df = df.groupby("topic")
+    system_count = len(pd.unique(df["system"]))
+
+    groups = []
 
     for _, group in grouped_df:
+        if len(group.index) == system_count:
+            groups.append(group)
+
+    for group in groups:
         max_fscore = group["avg_fscore"].max()
         systems_with_max_fscore = group[group["avg_fscore"] == max_fscore]["system"]
         systems_count = systems_with_max_fscore.value_counts().to_dict()
+
+        if len(group.index) != system_count:
+            continue
 
         # accumulate the counts for each system
         for system, count in systems_count.items():
@@ -16,13 +27,14 @@ def count_overall_systems_with_highest_avg_fscore(df):
                 overall_result_dict[system]["hit"] += count
             else:
                 overall_result_dict[system] = {}
+                overall_result_dict[system]["count"] = len(groups)
                 overall_result_dict[system]["hit"] = count
 
     # NOTE: Calc Averages
     for system, var in overall_result_dict.items():
-        count = len(df[df["system"] == system].index)
-        overall_result_dict[system]["count"] = count
-        overall_result_dict[system]["avg"] = var["hit"] / count
+        overall_result_dict[system]["avg"] = (
+            var["hit"] / overall_result_dict[system]["count"]
+        )
 
     return overall_result_dict
 
